8054f52cbd6cbbfc8200452739069433a08e1750
Izer6002 <rezianchabadze2006@gmail.com>
2022-12-25 00:01:31 +0400

    Added Player

---
+++ FootBall.API/Controllers/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootBall.API.Controllers
+{
+    using FootBall.API.Context;
+    using FootBall.API.Models;
+
+    using Microsoft.EntityFrameworkCore;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private PlayerContext db;
+
+        public PlayerController(PlayerContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<IEnumerable<Player>>> GetAll()
+        {
+            return await this.db.Player.ToListAsync();
+        }
+
+        [HttpGet("[action]/{id}")]
+        public async Task<ActionResult<Player>> GetById(int id)
+        {
+            Player player = await this.db.Player.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (player == null)
+                return NotFound();
+
+            return new ObjectResult(player);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<User>> Create(Player player)
+        {
+            if (player == null)
+                return this.BadRequest();
+
+            db.Player.Add(player);
+
+            await this.db.SaveChangesAsync();
+
+            return Ok(player);
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult<Player>> Update(Player player)
+        {
+            if (player == null)
+                return this.BadRequest();
+
+            if (!db.Player.Any(x => x.Id == player.Id))
+                return this.NotFound();
+
+            db.Player.Update(player);
+            await db.SaveChangesAsync();
+
+            return Ok(player);
+        }
+
+        [HttpDelete("[action]")]
+        public async Task<ActionResult<Player>> Delete(int id)
+        {
+            Player player = await this.db.Player.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (id < 0)
+                return NotFound();
+
+            db.Player.Remove(player);
+            await db.SaveChangesAsync();
+            return Ok(player);
+        }
+    }
+}
+
---
+++ FootBall.API/Entities/PlayerEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace FootBall.API.Entities
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public double Rating { get; set; }
+    }
+}
