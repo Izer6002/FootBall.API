9bf6d176309731322566ac9c4985a6bae30e69eb
Izer6002 <rezianchabadze2006@gmail.com>
2022-12-25 01:32:00 +0400

    Added Referee

---
+++ FootBall.API/Services/RefereeSevice.cs
@@ -0,0 +1,22 @@
+﻿namespace FootBall.API.Services
+{
+    using FootBall.API.Entities;
+    using FootBall.API.Interfaces;
+    public class RefereeService : IPRefereeService
+    {
+        public Referee GetConcrettePlayer(Referee referee)
+        {
+            if (referee == null || referee.Name.Length < 3 || referee.Secondname.Length < 5)
+            {
+                return null;
+            }
+            return referee;
+        }
+
+        public Referee GetHeadReferee(Referee referee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
