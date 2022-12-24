9e45a0ca11d2aa2fe0933ad2de52886a67ff18ce
Izer6002 <rezianchabadze2006@gmail.com>
2022-12-24 12:25:20 +0400

    Removed Commented Text From Program.cs

--- FootBall.API/Program.cs
+++ FootBall.API/Program.cs
@@ -12,14 +12,12 @@ builder.Services.AddDbContext<UserContext>(opt => opt.UseSqlServer(connection));
 builder.Services.AddControllers();
 
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 
 app.UseRouting();
 
